ba035a12f45450c9dfac8897ea553ab120d2fc3f
SabineEmbacher <sabine.embacher@brockmann-consult.de>
2019-08-07 08:46:52 +0200

    Update CreatingAnArray.java

--- src/main/java/com/bc/zarr/examples/CreatingAnArray.java
+++ src/main/java/com/bc/zarr/examples/CreatingAnArray.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class CreatingAnArray {
 
     public static void main(String[] args) throws IOException {
+        
         ZarrArray array = ZarrArray.create(new ArrayParams()
                 .withShape(10000, 10000).withChunks(1000, 1000).withDataType(DataType.i4)
         );
