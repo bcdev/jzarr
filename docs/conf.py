47e9ae234ead72ec8e0e7a146fd4e77b387bc640
sabine <sabine.embacher@brockmann-consult.de>
2019-08-08 16:15:07 +0200

    Prepare documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Sabine Embacher'
 author = 'Sabine Embacher'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = 'latest'
 
 
 # -- General configuration ---------------------------------------------------
