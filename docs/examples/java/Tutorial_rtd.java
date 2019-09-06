f68e07e327efdbffe719631701fd0d333265c5e4
sabine <sabine.embacher@brockmann-consult.de>
2019-09-06 16:21:31 +0200

    continue on documentation

--- docs/examples/java/Tutorial_rtd.java
+++ docs/examples/java/Tutorial_rtd.java
@@ -303,7 +303,7 @@ public class Tutorial_rtd {
     private static void example_15() throws IOException, InvalidRangeException {
         ZarrArray z = ZarrArray.create(new ArrayParams().shape(60).chunks(20).dataType(DataType.i4));
 
-        int[] dataShape = {20};
+        int[] dataShape = {30};
 
         int[] data1 = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
         int[] data2 = {40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69};
