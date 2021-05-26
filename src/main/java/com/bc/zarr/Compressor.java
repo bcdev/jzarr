5ef82eee7ba5cf24d0c5218ca0254a90245e4dc4
sabine <sabine.embacher@brockmann-consult.de>
2021-05-26 20:35:29 +0200

    Buffer size

--- src/main/java/com/bc/zarr/Compressor.java
+++ src/main/java/com/bc/zarr/Compressor.java
@@ -41,7 +41,7 @@ public abstract class Compressor {
     public abstract void uncompress(InputStream is, OutputStream os) throws IOException;
 
     void passThrough(InputStream is, OutputStream os) throws IOException {
-        final byte[] bytes = new byte[65536];
+        final byte[] bytes = new byte[4096];
         int read = is.read(bytes);
         while (read >= 0) {
             if (read > 0) {
