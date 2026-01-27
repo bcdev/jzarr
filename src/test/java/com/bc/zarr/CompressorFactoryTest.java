3267149fbc7e4a2cd0b13608d897fc8e7ada0e3a
TonioF <tonio.fincke@brockmann-consult.de>
2026-01-27 12:25:43 +0100

    adjusted test

--- src/test/java/com/bc/zarr/CompressorFactoryTest.java
+++ src/test/java/com/bc/zarr/CompressorFactoryTest.java
@@ -31,7 +31,7 @@ import org.junit.Test;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 public class CompressorFactoryTest {
